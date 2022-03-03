37183dada400aa494c9c6be8571319d1c6ccf510
Nicola Cortese <nicolacortese92@gmail.com>
2022-03-03 06:48:56 +0000

    corrected the oystercard_spec tests to reflect a starting balance of 10, also removed the required file of station as journey doesn't call on it itself

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,15 +5,15 @@ describe Oystercard do
     let(:oystercard) { Oystercard.new }
 
     it 'has a balance of ten' do
-        expect(oystercard.balance).to eq 0.0
+        expect(oystercard.balance).to eq 10.0
     end
 
     it 'top_up the balance' do
-        expect(oystercard.top_up(5)).to eq 5.0
+        expect(oystercard.top_up(5)).to eq 15.0
     end
 
     it 'raise an error when the maximum balance is exceeded, max_bal = 90' do
-        expect { oystercard.top_up(91) }.to raise_error "Maximum balance exceeded by 1.0"
+        expect { oystercard.top_up(81) }.to raise_error "Maximum balance exceeded by 1.0"
     end
 
     it 'reduces th balance by fare that needs to be deducted' do
